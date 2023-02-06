4ea8ee747b046057887a87a07bec3055d10e3831
Cindy Luo <pattern265@gmail.com>
2023-02-06 23:52:52 +0800

    feature: add skip test case sample

--- tests/test_first.py
+++ tests/test_first.py
@@ -1,9 +1,16 @@
-from config import API_HOST
+import pytest
+
+from config import API_HOST, ENV
+
 
 def inc(x):
     return x + 1
 
 
 def test_answer():
-    assert API_HOST == 'https://api.test.com'
     assert inc(4) == 5
+
+
+@pytest.mark.skipif("ENV == 'staging'")
+def test_production_host():
+    assert API_HOST == "https://api.test.com"
