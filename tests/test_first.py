cab3d6be65f883194f0f7ba7e911308ab71906ef
cindyluo <pattern265@gmail.com>
2024-06-20 15:32:17 +0800

    feature: run test in github actions

--- tests/test_first.py
+++ tests/test_first.py
@@ -1,6 +1,6 @@
 import pytest
 
-from config import API_HOST
+from config import API_HOST, DB_HOST
 
 
 def inc(x):
@@ -14,3 +14,13 @@ def test_answer():
 @pytest.mark.skip_by_env('staging')
 def test_production_host():
     assert API_HOST == "https://api.test.com"
+
+
+@pytest.mark.skip_by_env('staging')
+def test_production_db_host():
+    assert DB_HOST == "5.6.7.8"
+
+
+@pytest.mark.skip_by_env('production')
+def test_staging_db_host():
+    assert DB_HOST == "1.2.3.4"
